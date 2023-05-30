9e0bce5e7ba1deda6cd3d9b2763916edac512ce3
Aradhya Nepal <aradhya.1221@gmail.com>
2023-05-30 20:41:18 +0545

    Test and actual mode: 15 min work, either 15 sec rest or 5 min rest

--- lib/notification_screen.dart
+++ lib/notification_screen.dart
@@ -16,6 +16,8 @@ class _NotificationScreenState extends State<NotificationScreen> {
   bool isLoading = true;
   String value = "value";
 
+  bool isTestMode = false;
+
   @override
   void initState() {
     super.initState();
@@ -38,43 +40,77 @@ class _NotificationScreenState extends State<NotificationScreen> {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    isLoading = true;
-                    setState(() {});
-                    final sharedPref = await SharedPreferences.getInstance();
-                    if (isStarted) {
-                      await Workmanager().cancelAll();
-                    } else {
-                      await Workmanager().initialize(callbackDispatcher,
-                          isInDebugMode:
-                              true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+          : SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      isLoading = true;
+                      setState(() {});
+                      final sharedPref = await SharedPreferences.getInstance();
+                      if (isStarted) {
+                        await Workmanager().cancelAll();
+                      } else {
+                        await Workmanager().initialize(callbackDispatcher);
+                        if (isTestMode) {
+                          _testModeSetup();
+                        } else {
+                          Workmanager().registerPeriodicTask(
+                            "simpleTask",
+                            "Title",
+                            frequency: const Duration(minutes: 15),
+                            existingWorkPolicy: ExistingWorkPolicy.keep,
                           );
-
-                      const totalDuration=Duration(hours: 4);
-                      const interval=Duration(minutes: 10);
-                      for (Duration duration=Duration.zero;duration.inMilliseconds<=totalDuration.inMilliseconds;duration=Duration(milliseconds: duration.inMilliseconds+interval.inMilliseconds)){
-                        Workmanager().registerOneOffTask(
-                          "send_notification${duration.inMilliseconds}",
-                          "simpleTask",
-                          initialDelay: duration,
-                        );
+                        }
                       }
-                    }
-                    isLoading=false;
-                    isStarted=!isStarted;
-                    await sharedPref.setBool(value, isStarted);
-                    setState(() {});
-                  },
-                  child: Text(isStarted?"Stop":"Start"),
-                ),
-              ],
+                      isLoading = false;
+                      isStarted = !isStarted;
+                      await sharedPref.setBool(value, isStarted);
+                      setState(() {});
+                    },
+                    child: Text(isStarted ? "Stop" : "Start"),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Test Mode"),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Switch(
+                          value: isTestMode,
+                          onChanged: (value) {
+                            setState(() {
+                              isTestMode = value;
+                            });
+                          }),
+                    ],
+                  )
+                ],
+              ),
             ),
     );
+  }
+
+  void _testModeSetup() {
+    const totalDuration = Duration(minutes: 1);
+    const interval = Duration(seconds: 10);
+    for (Duration duration = Duration.zero;
+        duration.inMilliseconds <= totalDuration.inMilliseconds;
+        duration = Duration(
+            milliseconds: duration.inMilliseconds + interval.inMilliseconds)) {
+      Workmanager().registerOneOffTask(
+        "send_notification${duration.inMilliseconds}",
+        "simpleTask",
+        initialDelay: duration,
+      );
+    }
   }
 
   @override
@@ -82,4 +118,3 @@ class _NotificationScreenState extends State<NotificationScreen> {
     super.dispose();
   }
 }
-
--- lib/notification_work_manager.dart
+++ lib/notification_work_manager.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vibration/vibration.dart';
 import 'package:workmanager/workmanager.dart';
 
@@ -21,7 +22,13 @@ void callbackDispatcher() {
       await flutterLocalNotificationsPlugin.initialize(
         initializationSettings,
       );
-      const AndroidNotificationDetails androidNotificationDetails =
+      const hint='Close Your Eye for 30 seconds or look far object for 30 seconds';
+      final sharedPref=await SharedPreferences.getInstance();
+      await sharedPref.reload();
+      String key="ShortRest";
+      final isShortRest=sharedPref.getBool(key)??true;
+      final valueToShow=isShortRest?hint:"Take 5 min rest:\n1)$hint\n2)Water Fill\n3)Washroom\n4)Just evaluate what done and plan will will do by closing eyes\n5)Stretch.";
+      final AndroidNotificationDetails androidNotificationDetails =
           AndroidNotificationDetails('your channel id', 'your channel name',
               channelDescription: 'your channel description',
               importance: Importance.max,
@@ -29,15 +36,17 @@ void callbackDispatcher() {
               enableLights: true,
               enableVibration: true,
               colorized: true,
+              styleInformation:  BigTextStyleInformation(valueToShow),
               ticker: 'ticker');
-      const NotificationDetails notificationDetails =
+      final NotificationDetails notificationDetails =
       NotificationDetails(android: androidNotificationDetails);
       Logger().i("In Loop");
-      Vibration.vibrate(duration: 1000);
-      AudioPlayer()..play(AssetSource('audio/blink_time.mp3'))..resume();
+      Vibration.vibrate(duration: 1500);
+
       await flutterLocalNotificationsPlugin.show(
-          Random().nextInt(1000), 'plain title', 'plain body', notificationDetails,
+          Random().nextInt(1000), 'Rest Time', valueToShow, notificationDetails,
           payload: 'item x');
+      await sharedPref.setBool(key, !isShortRest);
     } catch (err) {
       Logger().e("Was here"); // Logger flutter package, prints error on the debug console
       Logger().e(err
