a8e5b5b8046ce8bbe4636c77663b292557c497c9
Aradhya Nepal <aradhya.1221@gmail.com>
2023-05-30 16:36:09 +0545

    Little Refractor

--- lib/notification_screen.dart
+++ lib/notification_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:vibration/vibration.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -11,10 +13,49 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  final Completer<NotificationData> _notificationInitialized = Completer();
+
+  void initialize() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_launcher');
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('your channel id', 'your channel name',
+            channelDescription: 'your channel description',
+            importance: Importance.max,
+            priority: Priority.max,
+            enableLights: true,
+            enableVibration: true,
+            colorized: true,
+            ticker: 'ticker');
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    _notificationInitialized.complete(
+      NotificationData(
+        flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin,
+        notificationDetails: notificationDetails,
+      ),
+    );
+  }
+
   Timer? timer;
+
   @override
   Widget build(BuildContext context) {
-    final bool timerStarted=timer!=null;
+    final bool timerStarted = timer != null;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notify Eye Rest"),
@@ -23,53 +64,49 @@ class _NotificationScreenState extends State<NotificationScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(
-              onPressed: ()async{
-                if(timerStarted){
-                  timer?.cancel();
-                  timer=null;
-                }else{
-                  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-                  FlutterLocalNotificationsPlugin();
-                  const AndroidInitializationSettings initializationSettingsAndroid =
-                  AndroidInitializationSettings('ic_launcher');
-                  const InitializationSettings initializationSettings = InitializationSettings(
-                      android: initializationSettingsAndroid,
-                  );
-                  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-                      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
-                  const AndroidNotificationDetails androidNotificationDetails =
-                  AndroidNotificationDetails('your channel id', 'your channel name',
-                      channelDescription: 'your channel description',
-                      importance: Importance.max,
-                      priority: Priority.high,
-                      ticker: 'ticker');
-                  const NotificationDetails notificationDetails =
-                  NotificationDetails(android: androidNotificationDetails);
-                  timer=Timer.periodic(const Duration(seconds: 10), (timer) async{
-                    await flutterLocalNotificationsPlugin.show(
-                        timer.tick, 'plain title', 'plain body', notificationDetails,
-                        payload: 'item x');
-                  });
-                }
-                setState(() {
-
+            onPressed: () async {
+              if (timerStarted) {
+                timer?.cancel();
+                timer = null;
+              } else {
+                timer =
+                    Timer.periodic(const Duration(seconds: 10), (timer) async {
+                  if (await Vibration.hasVibrator() == true) {
+                    Vibration.vibrate(duration: 5000, amplitude: 255);
+                  }
+                  final data=await _notificationInitialized.future;
+                  await data.flutterLocalNotificationsPlugin.show(timer.tick,
+                      'plain title', 'plain body', data.notificationDetails,
+                      payload: 'item x');
                 });
-              },
-              child: Text(
-                timerStarted?"Stop":"Start"
-              ),
+              }
+              setState(() {});
+            },
+            child: Text(timerStarted ? "Stop" : "Start"),
           ),
         ],
       ),
     );
   }
+
   @override
   void dispose() {
     timer?.cancel();
     super.dispose();
   }
 
-  void onDidReceiveNotificationResponse(NotificationResponse notificationResponse) async {
+  void onDidReceiveNotificationResponse(
+      NotificationResponse notificationResponse) async {
     print("Hello");
   }
+}
+
+class NotificationData {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  NotificationDetails notificationDetails;
+
+  NotificationData({
+    required this.flutterLocalNotificationsPlugin,
+    required this.notificationDetails,
+  });
 }
